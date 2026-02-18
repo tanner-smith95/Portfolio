55db5512b0bc9983bc9992f98b8a794abd05ae81
Tanner Smith <mountainbug95@gmail.com>
2026-02-18 12:43:59 -0500

    Template / add interactivity to the desktop page navigation

--- components/mollecules/pageNav/pageNav.stories.tsx
+++ components/mollecules/pageNav/pageNav.stories.tsx
@@ -6,6 +6,25 @@ const meta = {
     title: 'Components/Mollecules/PageNav',
     component: PageNav,
     tags: ['autodocs'],
+    decorators: [
+        (Story) => (
+            <div>
+                <Story />
+
+                <div data-nav-title="Section 1" style={{ height: '100vh' }}>
+                    <h2>Section 1</h2>
+                </div>
+
+                <div data-nav-title="Section 2" style={{ height: '100vh' }}>
+                    <h2>Section 2</h2>
+                </div>
+
+                <div data-nav-title="Section 3" style={{ height: '100vh' }}>
+                    <h2>Section 3</h2>
+                </div>
+            </div>
+        ),
+    ],
 } satisfies Meta<typeof PageNav>;
 
 export default meta;
--- components/mollecules/pageNav/pageNav.tsx
+++ components/mollecules/pageNav/pageNav.tsx
@@ -1,22 +1,52 @@
+import { useEffect, useRef, useState } from "react";
 import styles from "./page-nav.module.scss";
 
 
 export const PageNav = () => {
-    return (
-        <nav className={styles["page-nav-component"]}>
+    const navRef = useRef<HTMLElement>(null);
+    const [navItems, setNavItems] = useState<Element[]>([]);
+
+    useEffect(() => {
+
+        if (typeof document !== "undefined") {
+            const sections = document.querySelectorAll("[data-nav-title]");
+            setNavItems(Array.from(sections));
+        }
+    }, [])
+
+
+
+    return navItems.length ? (
+        <nav ref={navRef} className={styles["page-nav-component"]}>
             <div className={`${styles["inner"]} container-wide content-gutter`}>
-                <div className={styles["nav-item"]}>
-                    About Me
-                </div>
-                <div className={styles["nav-item"]}>
-                    Experience
-                </div>
-                <div className={styles["nav-item"]}>
-                    Contact Me
-                </div>
+                {navItems.map((item, index) => {
+                    return (
+                        <div
+                            className={styles["nav-item"]}
+                            key={`page-nav-item-${index}`}
+                            // Scroll to corresponding page element on click
+                            onClick={() => {
+                                (item as HTMLElement).style.scrollMarginTop =
+                                    `${(navRef?.current?.clientHeight || 0) + 16}px`; // Account for nav height offset
+                                item?.scrollIntoView({ behavior: "smooth" });
+                            }}
+                            role="button"
+                            tabIndex={0}
+                            // Handle Enter key presses
+                            onKeyDown={(e) => {
+                                if (e?.key === "Enter") {
+                                    const castElt = e?.target as HTMLElement;
+                                    castElt?.click();
+                                }
+                            }}
+                        >
+                            {item.getAttribute("data-nav-title")}
+                        </div>
+                    )
+                })}
             </div>
         </nav>
-    );
+    ) : null;
 }
 
 export default PageNav;
