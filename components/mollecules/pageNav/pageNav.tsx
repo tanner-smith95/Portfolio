c1259e9b5a26d03438a7f6739f139fe7703d2d6c
Tanner Smith <mountainbug95@gmail.com>
2026-02-18 13:10:25 -0500

    Add logic to handle the desktop navigation active item state

--- components/mollecules/pageNav/pageNav.tsx
+++ components/mollecules/pageNav/pageNav.tsx
@@ -5,6 +5,9 @@ import styles from "./page-nav.module.scss";
 export const PageNav = () => {
     const navRef = useRef<HTMLElement>(null);
     const [navItems, setNavItems] = useState<Element[]>([]);
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const NAV_OFFSET = 16; // Offset in pixels to trigger active state change when section overlaps with the nav
 
     useEffect(() => {
 
@@ -14,6 +17,35 @@ export const PageNav = () => {
         }
     }, [])
 
+    useEffect(() => {
+
+        window.addEventListener("scroll", () => {
+            const navRect = navRef?.current?.getBoundingClientRect();
+
+            // Always set the last nav item to active when scrolled to the bottom of the page
+            if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+                setActiveIndex(navItems.length - 1);
+
+                // Otherwise, set the active nav item based on section overlap with the nav
+            } else {
+                // Set active state based on section overlap with the nav
+                if (navRect) {
+                    for (let i = 0; i < navItems.length; i++) {
+                        const item = navItems[i];
+                        const itemRect = item.getBoundingClientRect();
+
+                        if (
+                            navRect.bottom > itemRect.top - NAV_OFFSET &&
+                            navRect.top < itemRect.bottom
+                        ) {
+                            setActiveIndex(i)
+                        }
+                    }
+                }
+            }
+        })
+    }, [navItems])
+
 
 
     return navItems.length ? (
@@ -22,12 +54,12 @@ export const PageNav = () => {
                 {navItems.map((item, index) => {
                     return (
                         <div
-                            className={styles["nav-item"]}
+                            className={`${styles["nav-item"]} ${activeIndex === index ? styles["active"] : ""}`}
                             key={`page-nav-item-${index}`}
                             // Scroll to corresponding page element on click
                             onClick={() => {
                                 (item as HTMLElement).style.scrollMarginTop =
-                                    `${(navRef?.current?.clientHeight || 0) + 16}px`; // Account for nav height offset
+                                    `${(navRef?.current?.clientHeight || 0) + NAV_OFFSET}px`; // Account for nav height offset
                                 item?.scrollIntoView({ behavior: "smooth" });
                             }}
                             role="button"
